6bd7272a1421577e45caefa49ef48c5b1a152898
Nikolay Astakhayev <astakhayev@yandex.ru>
2020-12-12 02:37:58 +0800

    refactor: подготовить для сборки Webpack-ом

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
-import { initialCards } from './initialCards.js'
-import { Card } from './Card.js';
-import { FormValidator } from './FormValidator.js';
-import { Section } from './Section.js'
-import { PopupWithImage } from './PopupWithImage.js';
-import { UserInfo } from './UserInfo.js';
-import { PopupWithForm } from './PopupWithForm.js';
+import './index.css';
+import { initialCards } from '../components/initialCards.js'
+import { Card } from '../components/Card.js';
+import { FormValidator } from '../components/FormValidator.js';
+import { Section } from '../components/Section.js'
+import { PopupWithImage } from '../components/PopupWithImage.js';
+import { UserInfo } from '../components/UserInfo.js';
+import { PopupWithForm } from '../components/PopupWithForm.js';
 
 
 const cardsContainer = '.elements';
