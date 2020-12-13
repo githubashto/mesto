aaeb9e38cdd8d1a2962da7fccf1e6e5eee8165ca
Nikolay Astakhayev <astakhayev@yandex.ru>
2020-12-13 21:11:41 +0800

    fix: вынести поиск элементов в конструтор класса UserInfo

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,16 +1,16 @@
 export class UserInfo {
   constructor({ profileNameSelector, profileProfessionSelector }) {
-    this._name = profileNameSelector;
-    this._profession = profileProfessionSelector;
+    this._name = document.querySelector(profileNameSelector);
+    this._profession =  document.querySelector(profileProfessionSelector);
   }
 
   getUserInfo() {
-    this._userData = {name: document.querySelector(this._name).textContent, profession: document.querySelector(this._profession).textContent};
+    this._userData = {name: this._name.textContent, profession: this._profession.textContent};
     return this._userData;
   }
 
   setUserInfo({name, profession}) {
-    document.querySelector(this._name).textContent = name;
-    document.querySelector(this._profession).textContent = profession;
+    this._name.textContent = name;
+    this._profession.textContent = profession;
   }
 }
