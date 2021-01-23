ab07cb5274c5c1381732e7b1cdd3161f95baf0a9
Nikolay Astakhayev <astakhayev@yandex.ru>
2021-01-23 23:29:02 +0800

    feat: убрать удаление на начальных карточках

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,8 @@
 export class Card {
-  constructor(name, link, cardSelector, handleCardClick) {
+  constructor(name, link, selector, handleCardClick) {
     this._name = name;
     this._link = link;
-    this._selector = cardSelector;
+    this._selector = selector;
     this._handleCardClick = handleCardClick;
   }
 
@@ -12,27 +12,28 @@ export class Card {
       .content
       .querySelector('.element')
       .cloneNode(true);
-
     return cardElement;
   }
 
   generateCard() {
     this._element = this._getTemplate();
+    this._addContent();
     this._setEventsListener();
+    return this._element;
+  }
+
+  _addContent() {
     const cardTitle = this._element.querySelector('.element__title');
     const cardImage = this._element.querySelector('.element__image');
     cardTitle.textContent = this._name;
     cardImage.src = this._link;
     cardImage.alt = this._name;
-    return this._element;
   }
 
   _setEventsListener() {
 // добавляем слушатели
     // — лайк/анлайк
     this._element.querySelector('.element__like').addEventListener('click', this._likeCard);
-    // — удаление карточки
-    this._element.querySelector('.element__delete').addEventListener('click', () => this._remove());
     // — превью изображения
     this._element.querySelector('.element__image').addEventListener("click", () => {
        this._handleCardClick(this._name, this._link);
